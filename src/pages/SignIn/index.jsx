5e8f3ac79ae0d309df900bbf564365fbddfba6de
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 17:03:38 -0300

    feat: add tasks page redirection

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import { ToastsStore } from 'react-toasts';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import UserForm from '../../components/UserForm';
 import localStorageHelper from '../../helpers/localStorageHelper';
 import Header from '../../components/Header';
 
 const { REACT_APP_API_URL } = process.env;
 
-const handleSignIn = async ({ email, password }) => {
-  try {
-    const response = await axios.post(`${REACT_APP_API_URL}/users/signin`, { email, password });
+const SignIn = () => {
+  const history = useHistory();
 
-    localStorageHelper.set('token', response.data.token);
+  const handleSignIn = async ({ email, password }) => {
+    try {
+      const response = await axios.post(`${REACT_APP_API_URL}/users/signin`, { email, password });
 
-    ToastsStore.success('Success!');
+      localStorageHelper.set('token', response.data.token);
 
-    // TODO: redirect to tasks page
-  } catch (error) {
-    ToastsStore.error(error.response.data.message);
-  }
+      ToastsStore.success('Success!');
+
+      history.push('/tasks');
+    } catch (error) {
+      ToastsStore.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <main>
+      <Header />
+
+      <h1 data-testid="signin-title">SignIn</h1>
+
+      <UserForm handleSubmit={handleSignIn} />
+    </main>
+  );
 };
-
-const SignIn = () => (
-  <main>
-    <Header />
-
-    <h1 data-testid="signin-title">SignIn</h1>
-
-    <UserForm handleSubmit={handleSignIn} />
-  </main>
-);
 
 export default SignIn;
