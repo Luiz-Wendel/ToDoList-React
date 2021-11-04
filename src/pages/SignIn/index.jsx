b66acf8e30cf6bde70adaffe52056c726c71b3ee
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 11:44:21 -0300

    feat: add email field

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const SignIn = () => (
-  <main>
-    <h1 data-testid="signin-title">SignIn</h1>
-  </main>
-);
+const SignIn = () => {
+  const [email, setEmail] = React.useState('');
+
+  return (
+    <main>
+      <h1 data-testid="signin-title">SignIn</h1>
+
+      <form>
+        <section>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email@domain.com"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+        </section>
+      </form>
+    </main>
+  );
+};
 
 export default SignIn;
