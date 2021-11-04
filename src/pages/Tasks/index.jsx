9d853e964f65ee9d6a0956d1e28ad62d8de9e6b2
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 15:30:47 -0300

    feat: add Tasks page

---
+++ src/pages/Tasks/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const Tasks = () => (
+  <main>
+    <Header />
+
+    <h1>Tasks</h1>
+  </main>
+);
+
+export default Tasks;
