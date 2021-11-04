60d5630cad72628502f73ca2ef4578387af32d9f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 13:58:37 -0300

    feat: add route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={SignIn} />
+    <Route exact path="/signup" component={SignUp} />
   </Switch>
 );
 
