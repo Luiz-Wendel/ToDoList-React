627edc3e824ebc63def261d5133ec0b63bcf1baf
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 16:00:35 -0300

    feat: add token presence validation

--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ToastsStore } from 'react-toasts';
 import Header from '../../components/Header';
+import localStorageHelper from '../../helpers/localStorageHelper';
 
-const Tasks = () => (
-  <main>
-    <Header />
+const Tasks = () => {
+  const [token, setToken] = React.useState('');
 
-    <h1>Tasks</h1>
-  </main>
-);
+  const history = useHistory();
+
+  React.useEffect(() => {
+    setToken(localStorageHelper.get('token'));
+  }, []);
+
+  React.useEffect(() => {
+    if (token === null) {
+      ToastsStore.error('Please sign in!');
+
+      history.push('/');
+    }
+  }, [token]);
+
+  return (
+    <main>
+      <Header />
+
+      <h1>Tasks</h1>
+    </main>
+  );
+};
 
 export default Tasks;
