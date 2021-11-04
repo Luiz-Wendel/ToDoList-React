570c9bd8772dc274418a8abe4c8eb2d4192148d5
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 20:27:37 -0300

    feat: add redirection to signin page when token error

--- src/components/NewTaskForm/index.jsx
+++ src/components/NewTaskForm/index.jsx
@@ -3,16 +3,25 @@ import PropTypes from 'prop-types';
 import { ToastsStore } from 'react-toasts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useHistory } from 'react-router-dom';
 import axiosHelper from '../../helpers/axiosHelper';
+import localStorageHelper from '../../helpers/localStorageHelper';
 
 const NewTaskForm = ({ token, setTasks }) => {
+  const history = useHistory();
   const [description, setDescription] = React.useState('');
 
   const handleNewTask = async () => {
     const data = await axiosHelper.postToApi('/tasks', { description }, token);
 
-    if (data.code) ToastsStore.error(data.message);
-    else {
+    if (data.code) {
+      ToastsStore.error(data.message);
+
+      if (data.code.endsWith('token')) {
+        localStorageHelper.set('token', '');
+        history.push('/');
+      }
+    } else {
       setTasks((previousTasks) => [...previousTasks, data]);
 
       ToastsStore.success('Created!');
--- src/components/TasksTable/index.jsx
+++ src/components/TasksTable/index.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { ToastsStore } from 'react-toasts';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import TaskRow from './TaskRow';
 import TableHead from './TableHead';
 import axiosHelper from '../../helpers/axiosHelper';
+import localStorageHelper from '../../helpers/localStorageHelper';
 
 const TasksTable = ({ token, tasks, setTasks }) => {
+  const history = useHistory();
+
   const getTasks = async () => {
     const data = await axiosHelper.getFromApi('/tasks', token);
 
     if (data.tasks) setTasks(data.tasks);
-    else ToastsStore.error(data.message);
+    else {
+      ToastsStore.error(data.message);
+
+      if (data.code.endsWith('token')) {
+        localStorageHelper.set('token', '');
+        history.push('/');
+      }
+    }
   };
 
   React.useEffect(() => {
