28083da736f14226663b9f792ac4713bf9103ac1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 17:58:10 -0300

    feat: add tasks table layout

---
+++ src/components/TasksTable/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableBody from './TableBody';
+import TableHead from './TableHead';
+
+const TasksTable = () => (
+  <table>
+    <TableHead />
+    <TableBody />
+  </table>
+);
+
+export default TasksTable;
