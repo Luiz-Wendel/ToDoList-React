7fee8235555ea3a483e71e559e05074f9194733c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-04 17:52:50 -0300

    feat: add table head

---
+++ src/components/TasksTable/TableHead/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TableHead = () => (
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Description</th>
+      <th>Status</th>
+      <th>Created At</th>
+    </tr>
+  </thead>
+);
+
+export default TableHead;
